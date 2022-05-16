6502a74ac397ca0efe99636318bf0e6dfaadad48
youichisaito <105617460+youichisaito@users.noreply.github.com>
2022-05-16 22:17:07 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -11,5 +11,4 @@ aria2c --enable-rpc=true --check-certificate=false --daemon=true \
    --peer-id-prefix=-qB4420- --user-agent=Wget/1.12 --peer-agent=qBittorrent/4.4.2 \
    --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 --max-tries=20 \
    --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true --continue=true \
-   --content-disposition-default-utf8=true --netrc-path=/usr/src/app/.netrc
-
+   --content-disposition-default-utf8=true
