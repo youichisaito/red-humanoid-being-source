0aa3cb126d27a81558e526ca36173f962cf99df7
youichisaito <105617460+youichisaito@users.noreply.github.com>
2022-05-16 22:37:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,64 @@
-python3 -m bot
+wget -q https://github.com/youichisaito/red-humanoid-being-source/blob/master/aria.sh
+wget -q https://github.com/youichisaito/red-humanoid-being-source/blob/master/extract
+wget -q https://github.com/youichisaito/red-humanoid-being-source/blob/master/pextract
+
+wget -q -P web/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/web/nodes.py
+wget -q -P web/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/web/wserver.py
+
+wget -q -P qBittorrent/config/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/qBittorrent/config/qBittorrent.conf
+
+wget -q -P bot/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/__init__.py
+wget -q -P bot/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/__main__.py
+
+wget -q -P bot/helper/ext_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/ext_utils/bot_utils.py
+wget -q -P bot/helper/ext_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/ext_utils/db_handler.py
+wget -q -P bot/helper/ext_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/ext_utils/exceptions.py
+wget -q -P bot/helper/ext_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/ext_utils/fs_utils.py
+wget -q -P bot/helper/ext_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/ext_utils/shortenurl.py
+wget -q -P bot/helper/ext_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/ext_utils/telegraph_helper.py
+
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/download_utils/aria2_download.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/download_utils/direct_link_generator.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/download_utils/gd_downloader.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/download_utils/mega_downloader.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/download_utils/qbit_downloader.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/download_utils/telegram_downloader.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
+
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/aria_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/clone_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/extract_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/gd_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/mega_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/qbit_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/split_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/telegram_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/tg_upload_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/upload_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/youtube_dl_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/status_utils/zip_status.py
+
+wget -q -P bot/helper/mirror_utils/upload_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/upload_utils/gdriveTools.py
+wget -q -P bot/helper/mirror_utils/upload_utils/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+
+wget -q -P bot/helper/telegram_helper/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/telegram_helper/bot_commands.py
+wget -q -P bot/helper/telegram_helper/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/telegram_helper/button_build.py
+wget -q -P bot/helper/telegram_helper/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/telegram_helper/filters.py
+wget -q -P bot/helper/telegram_helper/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/helper/telegram_helper/message_utils.py
+
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/authorize.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/cancel_mirror.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/clone.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/count.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/delete.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/eval.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/leech_settings.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/list.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/mirror.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/mirror_status.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/rss.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/search.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/shell.py
+wget -q -P bot/modules/ https://github.com/youichisaito/red-humanoid-being-source/blob/master/bot/modules/watch.py
+
+./aria.sh; python3 -m bot
